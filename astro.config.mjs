930216bcc6d6e640bf4a5f4a52303aded048b89e
unknown <derickgarcia1212@gmail.com>
2026-07-05 16:51:29 +0800

    added stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
-  base: 'virtual-exhibit-template',
+  site: 'https://virtual-exhibit-template.onrender.com',
+  base: '/',
 });
-
