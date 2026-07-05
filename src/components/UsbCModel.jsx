12f0c608ab6eaf557f12d7667fe004e16e802b4b
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-07-05 16:45:25 +0800

    Add files via upload

--- src/components/UsbCModel.jsx
+++ src/components/UsbCModel.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three-stdlib";
 
 export default function UsbCModel() {
   const mountRef = useRef(null);
